29c775ea726d80384cdd7388d317ff9ebd8fcc1d
CarlosSenra <crsbsenra@gmail.com>
2025-07-01 09:48:55 -0300

    reestruturacao folders user base

---
+++ src/app/models/usuario.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from app.shared.database.base import Base
+
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    nome = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
