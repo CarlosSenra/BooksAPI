b0f9a0f2cb318165846523b4a8cc4583be31f0a7
CarlosSenra <crsbsenra@gmail.com>
2025-07-02 20:48:43 -0300

    add Book db model

--- src/app/shared/database/models/books.py
+++ src/app/shared/database/models/books.py
@@ -1,1 +1,15 @@
 from sqlalchemy import Column, Integer, String, Float, Text, DateTime
+from app.shared.database.base import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    price = Column(Float)
+    availability = Column(String(100))
+    rating = Column(Integer)
+    image_url = Column(String(500))
+    link = Column(String(500))
+    category = Column(String(100))
